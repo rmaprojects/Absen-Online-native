2524fd8db3fba615137527f4f708d9d4cb616d6e
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-02-25 09:40:01 +0700

    update fastscroll

---
+++ app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/history/HistoryFragment.kt
@@ -0,0 +1,16 @@
+package com.pklproject.checkincheckout.ui.dashboard.history
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.pklproject.checkincheckout.R
+
+class HistoryFragment : Fragment(R.layout.fragment_history) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
