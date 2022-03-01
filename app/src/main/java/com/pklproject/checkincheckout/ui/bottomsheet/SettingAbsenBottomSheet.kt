bcff75c2ae5a77539855c67b4e1b2c4b79f0f9e3
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-01 14:45:11 +0700

    Finished Developing UI

--- app/src/main/java/com/pklproject/checkincheckout/ui/bottomsheet/SettingAbsenBottomSheet.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/bottomsheet/SettingAbsenBottomSheet.kt
@@ -1,5 +1,25 @@
 package com.pklproject.checkincheckout.ui.bottomsheet
 
-class SettingAbsenBottomSheet() {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.pklproject.checkincheckout.R
+
+class SettingAbsenBottomSheet : BottomSheetDialogFragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottomsheet_jadwal_absen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 
 }
