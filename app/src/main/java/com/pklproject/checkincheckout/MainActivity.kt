0bfc9e10ced23a412334b74bdd22525685722873
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-02 14:04:37 +0700

    Update MainActivity.kt

--- app/src/main/java/com/pklproject/checkincheckout/MainActivity.kt
+++ app/src/main/java/com/pklproject/checkincheckout/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.pklproject.checkincheckout
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -13,6 +16,7 @@ import com.pklproject.checkincheckout.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding:ActivityMainBinding by viewBinding()
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +27,34 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_dashboard, R.id.navigation_profile
+                R.id.navigation_dashboard, R.id.navigation_profile, R.id.navigation_history, R.id.navigation_settings
             )
         )
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            var showBottomNav: Boolean
+            var showTopAppBar: Boolean
+            when (destination.id) {
+                
+            }
+        }
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavView.setupWithNavController(navController)
     }
+
+    //Suport for back button
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    //For toolBar
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true
+    }
+
 }
