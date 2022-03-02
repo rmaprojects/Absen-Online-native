9fe622473e0d5cc1ed7e477868fe3cf308bc29dc
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-02 14:13:35 +0700

    Memberi TODO

--- app/src/main/java/com/pklproject/checkincheckout/MainActivity.kt
+++ app/src/main/java/com/pklproject/checkincheckout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -34,11 +35,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         )
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            var showBottomNav: Boolean
-            var showTopAppBar: Boolean
+            var showBottomNav = true
+            var showTopAppBar = true
             when (destination.id) {
-                
+                //Ketika navigation nya kesini, apakah di show atau di hide
+                //Kalau di hide = false,
+                //Kalau di show = true
+                R.id.navigation_dashboard -> {
+                    showBottomNav = false
+                    showTopAppBar = true
+                }
             }
+            binding.bottomNavView.isVisible = showBottomNav
+            binding.toolBar.isVisible = showTopAppBar
         }
 
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -56,5 +65,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         // Inflate the menu; this adds items to the action bar if it is present.
         return true
     }
+
+    //TODO semua ID destination diisi, mau di show atau tidak, petunjuk tertera di figma
+    //TODO semua class fragment di init bindingnya. Contoh sudah tertera di SettingsFragment
 
 }
--- app/src/main/java/com/pklproject/checkincheckout/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/settings/SettingsFragment.kt
@@ -3,9 +3,13 @@ package com.pklproject.checkincheckout.ui.settings
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.pklproject.checkincheckout.R
+import com.pklproject.checkincheckout.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private val binding: FragmentSettingsBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
