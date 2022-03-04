15afaec978b8d13fc179af51e9a923634fa7e727
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-04 09:49:42 +0700

    Update db_absensi.sql

--- api_absen/database/db_absensi.sql
+++ api_absen/database/db_absensi.sql
@@ -33,4 +33,18 @@ CREATE TABLE tbl_absensi (
     FOREIGN KEY (id_karyawan) REFERENCES tbl_karyawan(id_karyawan)
 );
 
-INSERT INTO tbl_karyawan()
+ALTER TABLE tbl_karyawan ADD COLUMN username VARCHAR (60);
+ALTER TABLE tbl_karyawan ADD COLUMN password VARCHAR (60);
+
+INSERT INTO tbl_karyawan(nama_karyawan ,jabatan ,departement ,business_unit ,status_admin)
+VALUES ("Mamang Sumamang", "Manager", "IT", "OMG Indonesia", true);
+
+INSERT INTO tbl_karyawan(nama_karyawan ,jabatan ,departement ,business_unit ,status_admin)
+VALUES ("Sumamang Sumang", "Manager", "IT", "OMG Indonesia", false);
+
+
+INSERT INTO tbl_absensi(id_karyawan, nama_karyawan,absen_pagi ,absen_siang ,absen_pulang ,absen_pagi_awal ,absen_pagi_akhir ,absen_siang_awal ,absen_siang_akhir ,absen_pulang_awal ,absen_pulang_akhir)
+VALUES (1, "Mamang Sumamang", "09:14:00", "15:12:00", "17:00:00", "09:00:00", "15:00:00", "15:00:00", "17:00:00", "17:00:00","22:00:00");
+
+INSERT INTO tbl_pengaturan_absen (absen_siang_diperlukan,absen_pagi_awal,absen_pagi_akhir,absen_siang_awal,absen_siang_akhir,absen_pulang_awal,absen_pulang_akhir)
+VALUES (true, "09:00:00", "15:00:00", "15:00:00", "17:00:00", "17:00:00","22:00:00");
