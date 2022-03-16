9aa5a2f1859c244218b41665891d9ea927cd1c86
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-16 09:01:19 +0700

    update navigation, add absensi

---
+++ app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/absen/AbsenMenuFragment.kt
@@ -0,0 +1,13 @@
+package com.pklproject.checkincheckout.ui.dashboard.absen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.pklproject.checkincheckout.R
+
+class AbsenMenuFragment : Fragment(R.layout.fragment_menu_absen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
