c08c7a909aed38c9aca74430736840af4dc64273
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-21 16:14:20 +0700

    post

--- app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
+++ app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
@@ -1,12 +1,83 @@
 package com.pklproject.checkincheckout.api.`interface`
 
+import com.pklproject.checkincheckout.api.models.AbsenModel
+import com.pklproject.checkincheckout.api.models.History
+import com.pklproject.checkincheckout.api.models.HistoryModel
+import com.pklproject.checkincheckout.api.models.Login
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import retrofit2.http.Body
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiInterface {
 
+    @POST("username")
+    suspend fun postusername(
+        @Body data: Login
+    ):Response<Unit>
+
+    @POST("password")
+    suspend fun postpassword(
+        @Body data: Login
+    ):Response<Unit>
+
+    @POST("username")
+    suspend fun postusername(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("password")
+    suspend fun postpassword(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("tipe_absen")
+    suspend fun posttipeabsen(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("longitude")
+    suspend fun postlongitude(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("latitude")
+    suspend fun postlatitude(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("photo_name")
+    suspend fun postphoto_name(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("keterangan")
+    suspend fun postketerangan(
+        @Body data: AbsenModel
+    ):Response<Unit>
+
+    @POST("username")
+    suspend fun postusername(
+        @Body data: HistoryModel
+    ):Response<Unit>
+
+    @POST("password")
+    suspend fun postpassword(
+        @Body data: HistoryModel
+    ):Response<Unit>
+
+    @POST("tahun")
+    suspend fun posttahun(
+        @Body data: HistoryModel
+    ):Response<Unit>
+
+    @POST("bulan")
+    suspend fun postbulan(
+        @Body data: HistoryModel
+    ):Response<Unit>
     companion object{
         private const val BASE_URL: String = "10.10.22.147/api_absen"
 
---
+++ app/src/main/java/com/pklproject/checkincheckout/api/models/AbsenSettingsModel.kt
@@ -0,0 +1,15 @@
+package com.pklproject.checkincheckout.api.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class AbsenSettingsModel(
+    @SerializedName("code")
+    val code: Int,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("setting")
+    val setting: List<Setting>,
+    @SerializedName("status")
+    val status: Boolean
+)
