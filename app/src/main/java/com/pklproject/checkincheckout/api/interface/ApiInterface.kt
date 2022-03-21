638292b419fcb2127e8456796b5519e165a3b8bc
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-21 15:07:52 +0700

    Update ApiInterface.kt

--- app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
+++ app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
@@ -1,6 +1,12 @@
 package com.pklproject.checkincheckout.api.`interface`
 
+import retrofit2.http.Query
+
 interface ApiInterface {
 
-    
+    @Query("SELECT * History")
+
+    @Query("SELECT * AbsenModel")
+
+    @Query("SELECT * Login")
 }
