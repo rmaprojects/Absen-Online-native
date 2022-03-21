a85264636e06a8d344d1b93daebc77971b704043
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-21 16:18:38 +0700

    Update ApiInterface.kt

--- app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
+++ app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
@@ -7,10 +7,6 @@ import com.pklproject.checkincheckout.api.models.Login
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import retrofit2.http.Body
-import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface ApiInterface {
 
@@ -79,15 +75,14 @@ interface ApiInterface {
         @Body data: HistoryModel
     ):Response<Unit>
     companion object{
-        private const val BASE_URL: String = "10.10.22.147/api_absen"
+        private const val BASE_URL: String = "10.10.22.147/api_absen/"
 
-        fun create(): ApiInterface{
-            val retrofit = Retrofit.Builder()
+        fun createApi(): ApiInterface {
+            return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(ApiInterface::class.java)
-            return retrofit
         }
     }
 }
