114c78a588abb73eb6e9924f5b816762e50972c4
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-21 15:18:03 +0700

    Update ApiInterface.kt

--- app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
+++ app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
@@ -1,12 +1,22 @@
 package com.pklproject.checkincheckout.api.`interface`
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @Query("SELECT * History")
+    companion object{
+        private const val BASE_URL: String = "10.10.22.147/api_absen"
 
-    @Query("SELECT * AbsenModel")
-
-    @Query("SELECT * Login")
+        fun create(): ApiInterface{
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiInterface::class.java)
+            return retrofit
+        }
+    }
 }
