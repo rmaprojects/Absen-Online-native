d3be8c2a164432583fa8e90d76f0ace31e0fdcb2
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-21 16:55:20 +0700

    post get

--- app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
+++ app/src/main/java/com/pklproject/checkincheckout/api/interface/ApiInterface.kt
@@ -5,6 +5,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiInterface {
@@ -17,69 +18,29 @@ interface ApiInterface {
     ) : Response<LoginModel>
 
     @POST("username")
-    suspend fun postusername(
-        @Body data: Login
-    ):Response<Unit>
+    suspend fun Absen(
+        @Body username:String,
+        @Body password:String,
+        @Body tipe_absen:String,
+        @Body longitude:Double,
+        @Body latitude:Double,
+        @Body photo_name:String,
+        @Body keterangan:String
+    ):Response<AbsenModel>
 
     @POST("password")
-    suspend fun postpassword(
-        @Body data: Login
-    ):Response<Unit>
+    suspend fun History(
+        @Body username:String,
+        @Body password:String,
+        @Body tahun:Int,
+        @Body Bulan:String
+    ):Response<HistoryModel>
 
-    @POST("username")
-    suspend fun postusername(
-        @Body data: AbsenModel
-    ):Response<Unit>
+    @GET("bulan")
+    suspend fun AbsenSettings(
+        @Body Setting: String
+    ):Response<AbsenSettingsModel>
 
-    @POST("password")
-    suspend fun postpassword(
-        @Body data: AbsenModel
-    ):Response<Unit>
-
-    @POST("tipe_absen")
-    suspend fun posttipeabsen(
-        @Body data: AbsenModel
-    ):Response<Unit>
-
-    @POST("longitude")
-    suspend fun postlongitude(
-        @Body data: AbsenModel
-    ):Response<Unit>
-
-    @POST("latitude")
-    suspend fun postlatitude(
-        @Body data: AbsenModel
-    ):Response<Unit>
-
-    @POST("photo_name")
-    suspend fun postphoto_name(
-        @Body data: AbsenModel
-    ):Response<Unit>
-
-    @POST("keterangan")
-    suspend fun postketerangan(
-        @Body data: AbsenModel
-    ):Response<Unit>
-
-    @POST("username")
-    suspend fun postusername(
-        @Body data: HistoryModel
-    ):Response<Unit>
-
-    @POST("password")
-    suspend fun postpassword(
-        @Body data: HistoryModel
-    ):Response<Unit>
-
-    @POST("tahun")
-    suspend fun posttahun(
-        @Body data: HistoryModel
-    ):Response<Unit>
-
-    @POST("bulan")
-    suspend fun postbulan(
-        @Body data: HistoryModel
-    ):Response<Unit>
     companion object{
         private const val BASE_URL: String = "10.10.22.147/api_absen/"
 
