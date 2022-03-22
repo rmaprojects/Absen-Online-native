e848cc9a68fabbf1d98dd123eabec2e19b7f0d7d
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-22 15:11:21 +0700

    update 1

--- app/src/main/java/com/pklproject/checkincheckout/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/profile/ProfileFragment.kt
@@ -5,7 +5,10 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.pklproject.checkincheckout.R
+import com.pklproject.checkincheckout.api.models.LoginModel
 import com.pklproject.checkincheckout.databinding.FragmentProfileBinding
+import com.pklproject.checkincheckout.ui.auth.LoginActivity
+import com.pklproject.checkincheckout.ui.settings.TinyDB
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
@@ -13,5 +16,12 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val tinyDB = TinyDB(requireContext())
+
+        binding.name.text = tinyDB.getObject(LoginActivity.KEYSIGNIN, LoginModel::class.java).namaKaryawan
+        binding.jabatan.text = tinyDB.getObject(LoginActivity.KEYSIGNIN,LoginModel::class.java).jabatan
+        binding.departemen.text = tinyDB.getObject(LoginActivity.KEYSIGNIN,LoginModel::class.java).departement
+        binding.unit.text = tinyDB.getObject(LoginActivity.KEYSIGNIN,LoginModel::class.java).businessUnit
     }
 }
