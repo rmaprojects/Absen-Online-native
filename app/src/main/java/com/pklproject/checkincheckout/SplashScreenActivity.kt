0a72affd67d49c0386b915d72373021151813fee
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2022-03-22 11:16:13 +0700

    update splash

--- app/src/main/java/com/pklproject/checkincheckout/SplashScreenActivity.kt
+++ app/src/main/java/com/pklproject/checkincheckout/SplashScreenActivity.kt
@@ -1,17 +1,26 @@
 package com.pklproject.checkincheckout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.pklproject.checkincheckout.databinding.ActivitySplashScreenBinding
+import com.pklproject.checkincheckout.ui.auth.LoginActivity
+import com.pklproject.checkincheckout.ui.settings.Preferences
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private val binding: ActivitySplashScreenBinding by viewBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        if (Preferences(this).isLoggedIn) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
 
     }
 }
