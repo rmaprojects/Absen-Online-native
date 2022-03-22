a7476bb7b8e5beb49c829daebd1795143c4d5fe8
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-22 13:33:48 +0700

    Update LoginActivity.kt

--- app/src/main/java/com/pklproject/checkincheckout/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/auth/LoginActivity.kt
@@ -9,52 +9,56 @@ import com.google.android.material.snackbar.Snackbar
 import com.pklproject.checkincheckout.MainActivity
 import com.pklproject.checkincheckout.R
 import com.pklproject.checkincheckout.api.`interface`.ApiInterface
+import com.pklproject.checkincheckout.api.models.LoginModel
 import com.pklproject.checkincheckout.databinding.ActivityLoginBinding
 import com.pklproject.checkincheckout.ui.settings.TinyDB
 import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
-    private val binding : ActivityLoginBinding by viewBinding()
+    private val binding: ActivityLoginBinding by viewBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val username = binding.username.editText?.text
-        val Password = binding.password.editText?.text
+        val password = binding.password.editText?.text
         val api = ApiInterface.createApi()
 
         binding.masuk.setOnClickListener {
             lifecycleScope.launch {
-//                val response = api.login()
+                val response = api.login(username.toString(), password.toString())
 
                 try {
                     Snackbar.make(binding.masuk, "Login Berhasil!", Snackbar.LENGTH_SHORT)
                         .setAction("Ok") {}
                         .show()
                     TinyDB(this@LoginActivity).putObject(
-                        KEYSIGNIN, SignInReturn(
-                            response.access_token,
-                            response.expires_in,
-                            response.refresh_token,
-                            response.token_type,
-                            response.user
-                        ))
-
-                    SignedInPreferences(this@LoginActivity).isSignedIn = true
+                        KEYSIGNIN,
+                        LoginModel(
+                            response.code(),
+                            response.body()!!.login,
+                            response.message(),
+                            response.body()!!.status
+                        )
+                    )
                     startActivity(Intent(this@LoginActivity, MainActivity::class.java))
                     finish()
-                } catch (e:Exception) {
-                    Snackbar.make(binding.masuk, "User credential salah! Silahkan perbaiki lalu coba lagi!", Snackbar.LENGTH_SHORT)
+                } catch (e: Exception) {
+                    Snackbar.make(
+                        binding.masuk,
+                        "User credential salah! Silahkan perbaiki lalu coba lagi!",
+                        Snackbar.LENGTH_SHORT
+                    )
                         .setAction("Ok") {}
                         .show()
                 }
-                }
             }
         }
-
-        //TODO: buat function Login
-        companion object {
-            const val KEYSIGNIN = "SIGNINKEY"
-        }
     }
+
+    //TODO: buat function Login
+    companion object {
+        const val KEYSIGNIN = "SIGNINKEY"
+    }
+}
 
