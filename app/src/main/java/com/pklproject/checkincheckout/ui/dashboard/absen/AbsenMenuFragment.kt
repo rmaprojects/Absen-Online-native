833c23040c3e21a35d682833a75a535cb75462af
zhafran14 <Zhafranarifin746@gmail.com>
2022-03-23 16:06:47 +0700

    update 4

--- app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/absen/AbsenMenuFragment.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/absen/AbsenMenuFragment.kt
@@ -2,17 +2,54 @@ package com.pklproject.checkincheckout.ui.dashboard.absen
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.pklproject.checkincheckout.R
+import com.pklproject.checkincheckout.api.models.LoginModel
 import com.pklproject.checkincheckout.databinding.FragmentMenuAbsenBinding
+import com.pklproject.checkincheckout.ui.auth.LoginActivity
+import com.pklproject.checkincheckout.ui.settings.TinyDB
 
 class AbsenMenuFragment : Fragment(R.layout.fragment_menu_absen) {
 
-    private val binding: FragmentMenuAbsenBinding by viewBinding(R.layout.fragment_menu_absen)
+    private val binding: FragmentMenuAbsenBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.pilihanAbsen.check(R.id.izin)
+
+
+        binding.pilihanAbsen.setOnCheckedChangeListener{ group, isChecked ->
+            when(isChecked){
+                R.id.absen ->{
+                binding.absensi.isVisible = true
+                binding.izindialog.isVisible = false
+            }
+            R.id.izin ->{
+                binding.izindialog.isVisible = true
+                binding.absensi.isVisible = false
+            }
+            R.id.cuti ->{
+                binding.izindialog.isVisible = true
+                binding.absensi.isVisible = false
+            }
+            }
+        }
+//        when(binding.pilihanAbsen.checkedRadioButtonId){
+//            R.id.absen ->{
+//                binding.absensi.isVisible = true
+//                binding.izindialog.isVisible = false
+//            }
+//            R.id.izin ->{
+//                binding.izindialog.isVisible = true
+//                binding.absensi.isVisible = false
+//            }
+//            R.id.cuti ->{
+//                binding.izindialog.isVisible = true
+//                binding.absensi.isVisible = false
+//            }
+//        }
 
         //TODO:
         //  Tambahkan Logic ketika radio button memilih Izin atau Cuti, tampilkan tab izin atau cuti nya.
