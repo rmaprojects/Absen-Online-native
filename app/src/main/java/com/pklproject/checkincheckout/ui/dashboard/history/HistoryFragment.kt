806b0a00ab32ecd024fa22fcf95875bb98383af3
zhafran14 <Zhafranarifin746@gmail.com>
2022-04-01 16:04:24 +0700

    recyclerView

--- app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/history/HistoryFragment.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/history/HistoryFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.pklproject.checkincheckout.R
 import com.pklproject.checkincheckout.databinding.FragmentHistoryBinding
+import com.pklproject.checkincheckout.ui.dashboard.history.item.HistoryItem
 
 class HistoryFragment : Fragment(R.layout.fragment_history) {
 
@@ -13,6 +14,7 @@ class HistoryFragment : Fragment(R.layout.fragment_history) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val adapter = HistoryItem()
         //TODO: Set adapter dari recyclerView di layout fragment_history dengan HistoryItem()
 
     }
--- app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/history/item/HistoryItem.kt
+++ app/src/main/java/com/pklproject/checkincheckout/ui/dashboard/history/item/HistoryItem.kt
@@ -1,8 +1,29 @@
 package com.pklproject.checkincheckout.ui.dashboard.history.item
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
-class HistoryItem () {
+class HistoryItem (): RecyclerView.Adapter<HistoryItem.HistoryFragment>() {
 
-    //TODO: hubungkan class HistoryItem ini dengan layout history_item.xml, class ini adalah class RecyclerView
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryFragment {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HistoryFragment, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class HistoryFragment(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+    }
+
+    //  TODO: hubungkan class HistoryItem ini dengan layout history_item.xml, class ini adalah class RecyclerView
 }
